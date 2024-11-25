25d8ca887a4628347039e94b2922a4781f997056
cbeAbishek <agsabhishek1907@gmail.com>
2024-11-25 23:45:37 +0530

    1st commit

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,13 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "hackatank-1834c.firebaseapp.com",
-  projectId: "hackatank-1834c",
-  storageBucket: "hackatank-1834c.appspot.com",
-  messagingSenderId: "986553465308",
-  appId: "1:986553465308:web:8b8a8b3ced07911e00f719",
-  measurementId: "G-2677BE5L48",
 };
 
 export const app = initializeApp(firebaseConfig);
